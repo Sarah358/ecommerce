40a6df3e851f849580e361d86e72578e6f046643
Sarah358 <njerinjuguna358@gmail.com>
2022-03-05 21:08:07 +0300

    Remove a hardcoded Django secret key

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)n5e-tw^ei9jnr7%56fdv_ogaxkzl)^9c*)y3@(+h-gv^bem=^'
+# SECRET_KEY = 'django-insecure-)n5e-tw^ei9jnr7%56fdv_ogaxkzl)^9c*)y3@(+h-gv^bem=^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
